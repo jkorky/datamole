91997c3ceae59c1d3a1d3f33376a97c6d18a223f
Ján Korkoš <jankorkos@gmail.com>
2026-06-30 13:36:31 +0200

    F2: load todo items from server and display in List

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,45 @@
+import { useEffect, useState } from "react";
 import { Container } from "./components/Container";
 import { Layout } from "./components/Layout";
 import { List } from "./components/List";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import './global.css';
+import "./global.css";
+import { ListItem } from "./components/ListItem";
 
-export const App = () => (
+type TodoItem = {
+    id: number;
+    label: string;
+    isDone: boolean;
+    createdAt: number;
+};
+
+export const App = () => {
+    const [items, setItems] = useState<TodoItem[]>([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/items")
+            .then((response) => response.json())
+            .then((data: TodoItem[]) => setItems(data));
+    }, []);
+
+    return (
         <Container>
             <Layout>
                 <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
-                <List />
+                <List>
+                    {items.map((item) => (
+                        <ListItem
+                            key={item.id}
+                            label={item.label}
+                            isDone={item.isDone}
+                            onItemLabelEdit={() => {}}
+                            onItemDoneToggle={() => {}}
+                            onItemDelete={() => {}}
+                        />
+                    ))}
+                </List>
                 <Footer />
             </Layout>
         </Container>
-);
+    );
+};
